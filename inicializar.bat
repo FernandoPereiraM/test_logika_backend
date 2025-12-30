90c0c2c83bab9f43b05d386abe4483872572099a
FernandoPereiraM <103136298+FernandoPereiraM@users.noreply.github.com>
2025-12-29 20:40:34 -0500

    update .bat

--- inicializar.bat
+++ inicializar.bat
@@ -2,27 +2,26 @@
 TITLE FastAPI Dev Environment
 set PYTHONUTF8=1
 
+REM Ir siempre al directorio del proyecto
+cd /d "%~dp0"
+
 echo [1/6] Iniciando Docker...
-cd /d "%~dp0..\test_logika_backend"
 docker-compose up -d
 
 echo [2/6] Esperando a la BD...
 timeout /t 10 /nobreak > NUL
 
 echo [3/6] Limpiando rastro de versiones...
-cd /d "%~dp0..\test_logika_backend"
 docker exec technical_test_db psql -U postgres -d technical_test -c "DROP TABLE IF EXISTS alembic_version CASCADE;"
 
 echo [4/6] Aplicando esquema...
-cd /d "%~dp0..\test_logika_backend"
-call env\Scripts\activate && alembic revision --autogenerate -m "tables"
-call env\Scripts\activate && alembic upgrade head
+call env\Scripts\activate
+alembic upgrade head
 
 echo [5/6] Ejecutando Seed...
-cd /d "%~dp0..\test_logika_backend"
-call env\Scripts\activate && python -m app.db.seed
+python -m app.db.seed
 
 echo [6/6] Iniciando Servidor...
-cd /d "%~dp0..\test_logika_backend"
-call env\Scripts\activate && uvicorn app.main:app --reload
+uvicorn app.main:app --reload
+
 pause
